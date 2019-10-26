b9e3dbb2f1d5a7da0e87775b49c65fee55787268
it <it@DESKTOP-KKAQ0RK>
2019-10-26 11:37:55 +0900

    처음 하는거

---
+++ javaShudy/src/ch07/Car1Main.java
@@ -0,0 +1,8 @@
+package ch07;
+public class Car1Main {
+public static void main(String[] args) {
+Car1 car = new Car1();
+System.out.println(car.color);
+System.out.println(car.door);
+}
+}
---
+++ javaShudy/src/ch5/ch5.java
@@ -0,0 +1,16 @@
+package ch5;
+
+public class ch5 {
+	public class ArrayExam3 {
+	}
+public static void main(String[] args) {
+	int[] numbers = {30000000,2,1,7,4};
+	
+	int min =Integer. MAX_VALUE;
+	
+	for(int i = 0; i < numbers.length; i++) {
+		if(number[i] > min[] || min ==0 )
+	}
+	
+}
+}
